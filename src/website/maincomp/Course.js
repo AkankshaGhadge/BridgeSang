a5b5bc10da89191e11e060ee41044305f3e58011
pranjali <pranjalisuskar@gmail.com>
2024-02-02 23:36:39 +0530

    ::::::

--- src/website/maincomp/Course.js
+++ src/website/maincomp/Course.js
@@ -178,31 +178,85 @@ const Course = () => {
 </div>
 {/* Courses End */}
 
-<div style={{ marginTop: '30px', marginLeft: '160px' }}>
+<div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+  <div className="container">
+    <div className="text-center">
+      <h6 className="section-title bg-white text-center text-primary px-3">Our Star Alumnus</h6>
+      <h1 className="mb-5">Our Star Alumnus</h1>
+    </div>
+  
+
   <AliceCarousel
     mouseTracking
     items={[
       <div
-        className="col-lg-4 col-md-4 col-sm-4 col-xs-4"
+        className="col-lg-4"
         style={{ marginRight: '0', marginLeft: '0' }} // Remove margin
         key="uniqueKey" // Add a unique key for each item
       >
-        <div className="shop-cat-box" style={{ height: '200px', width: '200px', justifyContent: 'space-between' }}>
-          <img
-            src='' // Add your image source here
-            alt='/'
-            className="slider-image"
-            style={{ height: '200px' }}
-          />
-          <a className="btn hvr-hover" href="#">
-            {/* {subslider.brand_name} */}
-          </a>
-        </div>
+         <div className="testimonial-item text-center">
+  <img className="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-3.jpg" style={{width: 80, height: 80}} />
+  <h5 className="mb-0">Client Name</h5>
+  <p>Profession</p>
+  {/* <div className="testimonial-text bg-light text-center p-4">
+    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+  </div> */}
+</div>
+
       </div>,
+
+<div
+        className="col-lg-4"
+        style={{ marginRight: '0', marginLeft: '0' }} // Remove margin
+        key="uniqueKey" // Add a unique key for each item
+      >
+    <div className="testimonial-item text-center">
+  <img className="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-4.jpg" style={{width: 80, height: 80}} />
+  <h5 className="mb-0">Client Name</h5>
+  <p>Profession</p>
+  {/* <div className="testimonial-text bg-light text-center p-4">
+    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+  </div> */}
+</div>
+
+
+      </div>,
+
+
+
+       <div
+       className="col-lg-4 "
+       style={{ marginRight: '0', marginLeft: '0' }} // Remove margin
+       key="uniqueKey" // Add a unique key for each item
+     >
+      <div className="testimonial-item text-center">
+  <img className="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-2.jpg" style={{width: 80, height: 80}} />
+  <h5 className="mb-0">Client Name</h5>
+  <p>Profession</p>
+  {/* <div className="testimonial-text bg-light text-center p-4">
+    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+  </div> */}
+</div>
+
+     </div>,
+      <div
+      className="col-lg-4 "
+      style={{ marginRight: '0', marginLeft: '0' }} // Remove margin
+      key="uniqueKey" // Add a unique key for each item
+    >
+      <div className="testimonial-item text-center">
+  <img className="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-1.jpg" style={{width: 80, height: 80}} />
+  <h5 className="mb-0">Client Name</h5>
+  <p>Profession</p>
+  {/* <div className="testimonial-text bg-light text-center p-4">
+    <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+  </div> */} 
+</div>
+
+                {/* </div> */}
+    </div>,
       // Add more items as needed, make sure to provide a unique key for each
     ]}
-    // onSlideChange={handleOnSlideChange}
-    // onSlideChanged={handleOnSlideChanged}
     responsive={{
       0: { items: 1 },
       576: { items: 2 },
@@ -215,9 +269,11 @@ const Course = () => {
     infinite
   />
 </div>
+</div>
+</div>
 
 
-    </div>
+    // </div>
   )
 }
 
